d813d929bba292cc7db79e73a27cd72048bf4c37
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-01-08 22:14:33 +0200

    add [sass , gulp-sas] pakages

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,17 @@ var gulp  = require('gulp');
 var concat = require('gulp-concat'); 
 var autoPrefixer = require('gulp-autoprefixer');
 var mapSources = require('gulp-sourcemaps');
+var sass = require('gulp-sass')(require('sass')) ;
 
 
 gulp.task('test' , async function(){
     console.log("test sucssesful ") ;
 });
 
-gulp.task('lib-css' , async function(){
-   return gulp.src('project/css/components/main.css')
+gulp.task('lib-sass' , async function(){
+   return gulp.src('project/sass/components/main.sass')
     .pipe(mapSources.init())
+    .pipe(sass())
     .pipe(autoPrefixer())
     .pipe(concat('M-all.css'))
     .pipe(mapSources.write('.'))
